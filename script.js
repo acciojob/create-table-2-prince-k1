49345434bf545caabd8322b3b421634beb4f5a87
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-26 02:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function createTable() {
     //Write your code here
+	let table = document.getElementById('myTable');
+		let rows = parseInt(prompt('Input number of rows'));
+	let column = parseInt(prompt('input number of columns'));
+	for(let i = 0; i < rows; i++){
+		let tr = document.createElement('tr');
+		for(let j = 0; j < column; j++){
+			let td = document.createElement('td');
+			td.innerText = `Row-${i} Column-${j}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
   
 }
